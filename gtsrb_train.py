86b7acadfd143dd19176c4d0f932c1c604f8ce32
messi <bh_binghu@qq.com>
2018-02-01 17:57:35 +0800

    remove mnist_xx.py; rewrite LeNet

--- gtsrb_train.py
+++ gtsrb_train.py
@@ -6,18 +6,128 @@ import json
 import random
 import cv2
 import time
+import cPickle as pickle
 
 import tensorflow as tf
+from tensorflow.contrib.layers import flatten
 
 # 加载mnist_inference.py中定义的常量和前向传播的函数
 import gtsrb_inference
 
 # 配置神经网络的参数
 BATCH_SIZE = 2
+EPOCHS = 30000
+
+
+def LeNet(x):
+    mu = 0
+    sugma = 0.1
+
+    # LAYER 1:
+    conv1_w = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean=mu, stddev=sigma))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1   = tf.nn.conv2d(x, conv1_w, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+
+    conv1   = tf.nn.relu(conv1)
+
+    conv1   = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    # LAYER 2:
+    conv2_w = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2   = tf.nn.conv2d(x, conv1, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+
+    conv2   = tf.nn.relu(conv2)
+
+    conv2   = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+
+    fc0     = flatten(conv2)
+
+    fc1_w   = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
+    fc1_b   = tf.Variable(tf.zeros(120))
+    fc1     = tf.matmul(fc0, fc1_w) + fc1_b
+
+    fc1     = tf.nn.relu(fc1)
+
+    fc2_w   = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
+    fc2_b   = tf.Variable(tf.zeros(120))
+    fc2     = tf.matmul(fc1, fc2_w) + fc2_b
+
+    fc2     = tf.nn.relu(fc2)
+
+    fc3_w   = tf.Variable(tf.truncated_normal(shape=(84, 10), mean=mu, stddev=sigma))
+    fc3_b   = tf.Variable(tf.zeros(120))
+    fc3     = tf.matmul(fc2, fc3_w) + fc3_b
+
+    logits  = fc3
+    return logits
+
+def load_data():
+    from tensorflow.examples.tutorials.mnist import input_data
+    mnist = input_data.read_data_sets("./dataset/MNIST_data/", reshape=False)
+    X_train, y_train = mnist.train.images, mnist.train.labels
+    X_validation, y_validation = mnist.validation.images, mnist.validation.labels
+    X_test, y_test = mnist.test.images, mnist.test.labels
+    return X_train, y_train, X_validation, y_validation, X_test, y_test
+
+X_train, y_train, X_validation, y_validation, X_test, y_test = load_data()
+
+x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, 10)
+
+rate = 0.001
+
+logits = LeNet(x)
+
+cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, one_hot_y)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate=rate)
+train_operation = optimizer.minimize(loss_operation)
+
+
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset : offset + BATCH_SIZE], y_data[offset : offset + BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x:batch_x, y:batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+with tf.Session as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+
+    print("Training...")
+    print()
+
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset : end], y_train[offset : end]
+            sess.run(train_operation, feed_dict={x: batch_x, y: batch_y})
+
+        validation_accuracy = evaluate(X_validation, y_validation)
+        print("EPOCH {0}/{1} ...".format(i+1, EPOCHS))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+
+    try:
+        saver
+    except NameError:
+        saver = tf.train.Saver()
+    saver.save("sess", 'lenet')
+    print("Model saved")
+
+exit(0)
+
 LEARNING_RATE_BASE = 0.0001
 LEARNING_RATE_DECAY = 0.99
 REGULARAZTION_RATE = 0.0001
-TRAINING_STEPS = 30000
 MOVING_AVERAGE_DECAY = 0.99
 # 模型保存的路径和文件名
 MODEL_SAVE_PATH = "model/"
@@ -58,7 +168,7 @@ def train(gtsrb_items):
         tf.global_variables_initializer().run()
 
         # 验证和测试的过程将会有一个独立的程序来完成
-        for i in range(TRAINING_STEPS):
+        for i in range(EPOCHS):
             xs = np.zeros((BATCH_SIZE, gtsrb_inference.IMAGE_SIZE, gtsrb_inference.IMAGE_SIZE, gtsrb_inference.NUM_CHANNELS))
             ys = np.zeros((BATCH_SIZE, gtsrb_inference.NUM_LABELS))
             for j in range(BATCH_SIZE):
@@ -80,7 +190,7 @@ def train(gtsrb_items):
             # print xs.shape, ys.shape
             _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: xs, y_: ys})
             #每1000轮保存一次模型。
-            if i%1000 == 0:
+            if (i%1000 == 0) or (i == EPOCHS-1):
                 # 输出当前的训练情况。这里只输出了模型在当前训练batch上的损失函数大小。通过损失函数的大小可以大概了解训练的情况。
                 # 在验证数据集上的正确率信息会有一个单独的程序来生成。
                 time_str = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
@@ -88,55 +198,15 @@ def train(gtsrb_items):
                 # 保存当前的模型。注意这里隔出了global_step参数，这样可以让每个被保存模型的文件名末尾加上训练的轮数，比如“model.ckpt-1000”表示训练1000轮后得到的模型
                 saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
 
-def generate_json_file(train_json_fn, test_json_fn):
-    dataset_path = os.path.join(".", "dataset", "GTSRB", "Final_Training", "Images")
-    subdirs = os.listdir(dataset_path)
-    gtsrb_train_items = []
-    gtsrb_test_items = []
-    item_count = 0
-    for subdir in subdirs:
-        fulldir = os.path.join(dataset_path, subdir)
-        subfns = os.listdir(fulldir)
-        image_fns = [os.path.join(fulldir, fn) for fn in subfns if fn.endswith('.ppm')]
-        item_count += len(image_fns)
-        annotations_fns = [os.path.join(fulldir, fn) for fn in subfns if fn.endswith('.csv')]
-        assert(len(annotations_fns) == 1)
-        assert(len(image_fns)+len(annotations_fns) == len(subfns))
-        annotations_fn = annotations_fns[0]
-        gtsrb_items = []
-        with open(annotations_fn, 'r') as fd:
-            lines = fd.readlines()
-            for i, line in enumerate(lines):
-                params = line.replace("\r", '').replace('\n', '').split(';')
-                if i > 0:
-                    fn, w, h, x1, y1, x2, y2, class_id = params
-                    fn, w, h, x1, y1, x2, y2, class_id = fn, int(w), int(h), int(x1), int(y1), int(x2), int(y2), int(class_id)
-                    item = {}
-                    item['image_fn'] = os.path.join(fulldir, fn)
-                    item['size'] = (w, h)
-                    item['bbox'] = (x1, y1, x2, y2)
-                    item['class_id'] = class_id
-                    gtsrb_items.append(item)
-        test_cnt = int(len(gtsrb_items) * 0.1)
-        test_items = [gtsrb_items.pop(random.randint(0, len(gtsrb_items) - 1)) for i in range(test_cnt)]
-
-        gtsrb_test_items += test_items
-        gtsrb_train_items += gtsrb_items
-    with open(train_json_fn, 'w') as fd:
-        json.dump(gtsrb_train_items, fd)
-    with open(test_json_fn, 'w') as fd:
-        json.dump(gtsrb_test_items, fd)
-
 def main(argv=None):
-    train_json_fn = './gtsrb_train.json'
-    test_json_fn = './gtsrb_test.json'
-    if not os.path.isfile(train_json_fn):
-        generate_json_file(train_json_fn, test_json_fn)
-    
-    with open(train_json_fn, 'r') as fd:
-        gtsrb_train_items = json.load(fd)
-    train(gtsrb_train_items)
-
+    # train length : 34799
+    # test length  : 12630
+    # valid length :  4410
+    train_fn = './dataset/valid.2.p'
+    with open(train_fn, 'rb') as fd:
+        train_data = pickle.load(fd)
+        print train_data.keys()
+        print len(train_data['labels']), len(train_data['features']), len(train_data['coords']), len(train_data['sizes'])
 
 if __name__ == '__main__':
     tf.app.run()

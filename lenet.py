583e3bf9814315c12d6e5a9eebc0110acf10998b
figo_dev <figo_dev@jhhk.com>
2018-02-02 12:48:40 +0800

    add hist image

--- lenet.py
+++ lenet.py
@@ -19,12 +19,8 @@ data_source = 'gtsrb'  # 'gtsrb' or mnist
 
 if data_source == 'gtsrb':
     n_classes = 43
-    IMAGE_SIZE = 32
-    IMAGE_CHANNEL = 3
 elif data_source == 'mnist':
     n_classes = 10
-    IMAGE_SIZE = 32
-    IMAGE_CHANNEL = 1
 else:
     print("Unsupport data source. gtsrb or mnist is supported.")
     exit(-1)
@@ -35,13 +31,15 @@ def LeNet(x):
     sigma = 0.1
 
     # LAYER 1:
-    conv1_w = tf.Variable(tf.truncated_normal(shape=(5, 5, IMAGE_CHANNEL, 6), mean=mu, stddev=sigma))
+    conv1_w = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6), mean=mu, stddev=sigma))
     conv1_b = tf.Variable(tf.zeros(6))
     conv1   = tf.nn.conv2d(x, conv1_w, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
 
     conv1   = tf.nn.relu(conv1)
 
     conv1   = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    print conv1.shape
+
 
     # LAYER 2:
     conv2_w = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
@@ -51,21 +49,26 @@ def LeNet(x):
     conv2   = tf.nn.relu(conv2)
 
     conv2   = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    print conv2.shape
 
+    # LAYER 3
     fc0     = flatten(conv2)
 
+    # LAYER 4
     fc1_w   = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
     fc1_b   = tf.Variable(tf.zeros(120))
     fc1     = tf.matmul(fc0, fc1_w) + fc1_b
 
     fc1     = tf.nn.relu(fc1)
 
+    # LAYER 5
     fc2_w   = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
     fc2_b   = tf.Variable(tf.zeros(84))
     fc2     = tf.matmul(fc1, fc2_w) + fc2_b
 
     fc2     = tf.nn.relu(fc2)
 
+    # LAYER 6
     fc3_w   = tf.Variable(tf.truncated_normal(shape=(84, n_classes), mean=mu, stddev=sigma))
     fc3_b   = tf.Variable(tf.zeros(n_classes))
     fc3     = tf.matmul(fc2, fc3_w) + fc3_b
@@ -98,24 +101,69 @@ def load_data(source='mnist'):
         return None, None, None, None, None, None
     return X_train, y_train, X_validation, y_validation, X_test, y_test
 
+def preprocess_image(image):
+    shape = image.shape
+    if len(shape) == 2:
+        shape = (32, 32, 1)
+    else:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        shape = (32, 32, 1)
+    image = cv2.equalizeHist(image)
+    min_val = np.min(image) * 1.0
+    max_val = np.max(image) * 1.0
+    image = (image - min_val) / max_val - 0.5
+    return image.reshape(shape)
+
 X_train, y_train, X_validation, y_validation, X_test, y_test = load_data(data_source)
-# TODO: Number of training examples
+try:
+    from matplotlib import pyplot as plt
+    plt.figure(1)
+    plt.hist(y_train)
+    plt.xlabel("class")
+    plt.ylabel("count")
+    plt.title("number of each calss samples in train dataset")
+    plt.savefig("./examples/hist_train.png")
+
+    plt.figure(2)
+    plt.hist(y_validation)
+    plt.title("number of each calss samples in validation dataset")
+    plt.xlabel("class")
+    plt.ylabel("count")
+    plt.savefig("./examples/hist_validation.png")
+    plt.figure(3)
+    plt.hist(y_test)
+    plt.xlabel("class")
+    plt.ylabel("count")
+    plt.title("number of each calss samples in test dataset")
+    plt.savefig("./examples/hist_test.png")
+    # plt.show()
+except Exception, err:
+    print(err.message)
+#     pass
+# for i in range(10):
+#     index = random.randint(0, len(X_train))
+#     image = X_train[index]
+#     cv2.imshow('image', image)
+#     cv2.imshow('preprocess', preprocess_image(image))
+#     cv2.waitKey(0)
+# exit(0)
+# Number of training examples
 n_train = len(X_train)
 
-# TODO: Number of validation examples
+# Number of validation examples
 n_validation = len(X_validation)
 
-# TODO: Number of testing examples.
+# Number of testing examples.
 n_test = len(X_test)
 
-# TODO: What's the shape of an traffic sign image?
+# the shape of an traffic sign image:
 image_shape = (32, 32, 3)
 
 print("Number of train samples:%d" % n_train)
 print("Number of test samples:%d" % n_test)
 print("number of validation samples:%d" % n_validation)
 
-x = tf.placeholder(tf.float32, (None, 32, 32, IMAGE_CHANNEL))
+x = tf.placeholder(tf.float32, (None, 32, 32, 1))
 y = tf.placeholder(tf.int32, (None))
 one_hot_y = tf.one_hot(y, n_classes)
 
@@ -128,15 +176,10 @@ loss_operation = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate=rate)
 train_operation = optimizer.minimize(loss_operation)
 
+tf.summary.scalar('mean', loss_operation)
 
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-def preprocess_image(image):
-    shape = image.shape
-    image = cv2.resize(image, (32, 32))
-    image = image / 255.0
-    return image.reshape(shape)
 
 def evaluate(X_data, y_data):
     num_examples = len(X_data)
@@ -145,7 +188,7 @@ def evaluate(X_data, y_data):
     for offset in range(0, num_examples, BATCH_SIZE):
         batch_x, batch_y = X_data[offset : offset + BATCH_SIZE], y_data[offset : offset + BATCH_SIZE]
         n, w, h, c = batch_x.shape
-        new_batch_x = np.zeros((n, 32, 32, c))
+        new_batch_x = np.zeros((n, 32, 32, 1))
         for j in range(n):
             new_batch_x[j] = preprocess_image(batch_x[j])
         accuracy = sess.run(accuracy_operation, feed_dict={x:new_batch_x, y:batch_y})
@@ -166,7 +209,7 @@ with tf.Session() as sess:
             end = offset + BATCH_SIZE
             batch_x, batch_y = X_train[offset : end], y_train[offset : end]
             n, w, h, c = batch_x.shape
-            new_batch_x = np.zeros((n, 32, 32, c))
+            new_batch_x = np.zeros((n, 32, 32, 1))
             for j in range(n):
                 new_batch_x[j] = preprocess_image(batch_x[j])
             sess.run(train_operation, feed_dict={x: new_batch_x, y: batch_y})
--- writeup_report.md
+++ writeup_report.md
@@ -19,7 +19,9 @@ The goals / steps of this project are the following:
 
 [//]: # (Image References)
 
-[image1]: ./examples/visualization.jpg "Visualization"
+[image_train]: ./examples/hist_train.png "Visualization"
+[image_valid]: ./examples/hist_validation.png "Visualization"
+[image_test]: ./examples/hist_test.png "Visualization"
 [image2]: ./examples/grayscale.jpg "Grayscaling"
 [image3]: ./examples/random_noise.jpg "Random Noise"
 [image4]: ./examples/traffic_sign_0.png "Traffic Sign 1"
@@ -42,8 +44,7 @@ You're reading it! and here is a link to my [project code](https://github.com/me
 
 #### 1. Provide a basic summary of the data set. In the code, the analysis should be done using python, numpy and/or pandas methods rather than hardcoding results manually.
 
-I used the pandas library to calculate summary statistics of the traffic
-signs data set:
+I used the pandas library to calculate summary statistics of the traffic signs data set:
 Number of training examples:
 n_train = 34799
 
@@ -53,23 +54,25 @@ n_validation = 4410
 Number of testing examples.
 n_test = 12630
 
-# The shape of an traffic sign image:
+The shape of an traffic sign image:
 image_shape = (32, 32, 3)
 
-# The number of unique classes/labels in the dataset is
-n_classes = 43d
+The number of unique classes/labels in the dataset is
+n_classes = 43
 
 #### 2. Include an exploratory visualization of the dataset.
 
-Here is an exploratory visualization of the data set. It is a bar chart showing how the data ...
+Here is an exploratory visualization of the data set. It is a bar chart showing number of each class in 3 datasets.
 
-![alt text][image1]
+![alt text][image_train]
+![alt text][image_valid]
+![alt text][image_test]
 
 ### Design and Test a Model Architecture
 
 #### 1. Describe how you preprocessed the image data. What techniques were chosen and why did you choose these techniques? Consider including images showing the output of each preprocessing technique. Pre-processing refers to techniques such as converting to grayscale, normalization, etc. (OPTIONAL: As described in the "Stand Out Suggestions" part of the rubric, if you generated additional data for training, describe why you decided to generate additional data, how you generated the data, and provide example images of the additional data. Then describe the characteristics of the augmented training set like number of images in the set, number of images for each class, etc.)
 
-As a first step, I decided to convert the images to grayscale because ...
+As a first step, I changed the in put shape to (50x50). beacuse most of didn't convert the images to grayscale because graysacle image would lost color information. Most
 
 Here is an example of a traffic sign image before and after grayscaling.
 
